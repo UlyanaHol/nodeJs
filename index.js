88fa6ec017da70599cd8674f5a6753c6e28112f0
UlyanaHol <uluana.list.ru6942@gmail.com>
2023-03-27 20:35:18 +0300

    add formidable and change post/put/delete methods

--- index.js
+++ index.js
@@ -1,25 +1,65 @@
 const express = require('express');
 const app = express();
+const formidable = require('formidable');
 
 app.listen(3000, () => {
     console.log('Server has started on port 3000...');
 });
 
 //получение списка всех митапов
-app.get('/', (req, res) => {
-    meetups.forEach(meetup => {
-        res.send(meetup.show());
-    });
+app.get('/meetups', (req, res) => {
+    res.send(meetups);
 });
 
 //получение элемента по id
-app.get('/:id', (req, res) => {
+app.get('/meetups/:id', (req, res) => {
     meetups.forEach(el => {
         if (el.id == req.params.id) {
             res.send(el);
         }
     });
 });
+
+//регистрация нового митапа
+app.post('/meetups', (req, res) => {
+    //req.body
+    const form = formidable({ multiples: true });
+    form.parse(req, (err, fields, files) => {
+        let m = new MeetUp(fields.get(id));
+        meetups.push(m);
+        res.status(204);
+        res.end();
+    });
+});
+
+//изменение записи
+app.put('/meetups/:id', (req, res) => {
+    meetups.forEach(el => {
+        if (el.id == req.params.id) {
+            form.parse(req, (err, fields, files) => {
+                el.title = fields.get('id').title;
+                el.description = fields.get('id').description;
+                el.tags = fields.get('id').tags;
+                el.timeAndLocation = fields.get('id').timeAndLocation;
+                res.status(201);
+                res.end();
+            });
+        }
+    });
+});
+
+app.delete('/meetups/:id', (req, res) => {
+    meetups.forEach((el, index) => {
+        if (el.id == req.params.id) {
+            meetups.splice(index, 1);
+            res.status(201);
+            res.end();
+        }
+    });
+});
+
+
+
 //логика приложения, без базы данных пока
 class MeetUp {
     //конструктор
