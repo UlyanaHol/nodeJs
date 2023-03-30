e1c3c174901220d608fc0d381d543261ea69102b
UlyanaHol <uluana.list.ru6942@gmail.com>
2023-03-30 21:15:03 +0300

    change createMeetup function

--- controller/meetup.controller.js
+++ controller/meetup.controller.js
@@ -4,8 +4,20 @@ class MeetUpController {
     async createMeetup(req, res) {
         const {title, descr, tags, timeAndLocation} = req.body;
         console.log(title, descr, tags, timeAndLocation);
-        const newMeetUp = await db.query("INSERT INTO meetup {title, descr, tags, timeAndLocation} VALUES {$1, $2, $3, $4} RETURNING *", [title, descr, tags, timeAndLocation]);
-        res.json(ok);
+        const newMeetUp = await db.query("INSERT INTO meetup (title, descr, tags, timeAndLocation) VALUES ($1, $2, $3, $4) RETURNING id", [title, descr, tags, timeAndLocation]);
+        let m = new MeetUp(newMeetUp.rows[0].id, title, descr, tags, timeAndLocation);
+        res.json(m);
+    }
+}
+
+class MeetUp {
+    //конструктор
+    constructor(id, title, description, tags, timeAndLocation) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.tags = tags;
+        this.timeAndLocation = timeAndLocation;
     }
 }
 
